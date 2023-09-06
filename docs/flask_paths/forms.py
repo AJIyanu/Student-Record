3f09cf9d840d9ccd3137362d5e2c63d304005719
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-09-06 17:49:13 +0100

    registration for incomplete

--- docs/flask_paths/forms.py
+++ docs/flask_paths/forms.py
@@ -3,24 +3,28 @@
 """
 # import base64
 # from datetime import datetime
+import sys
 import json
+from importlib import import_module
 
 from flask_paths import app_views
 from flask import render_template, request, redirect, jsonify
-# from flask import session, make_response
+from flask import make_response
+
+sys.path.insert(0, 'C:\\Users\\adere\\Documents\\GitHub\\AJIyanu\\Mesacot\\Student Record')
+# print(sys.path)
+Auth = import_module("models.persons.auth").Auth
+Persons = import_module("models.persons.person").Persons
+Student = import_module("models.persons.students").Student
+Lecturer = import_module("models.persons.lecturers").Lecturer
+Admin = import_module("models.persons.admin").Admin
 
 
-# from models.persons.auth import Auth
-from models.persons.person import Persons
-# from ..models.persons.students import Student
-# from ..models.persons.lecturers import Lecturer
-# from ..models.persons.admin import Admin
-
-# Persons_dict = {
-#                 "student": Student,
-#                 "lecturer": Lecturer,
-#                 "admin": Admin
-#                 }
+Persons_dict = {
+                "student": Student,
+                "lecturer": Lecturer,
+                "admin": Admin
+                }
 
 @app_views.route('/signin', methods=['POST'])
 def login():
@@ -34,4 +38,13 @@ def login():
     else:
         return jsonify({"error": "Invalid username or passowrd"})
     userdata = json.loads(user.json_me())
-    return jsonify(user=userdata, status="succesful log in")
+    # return jsonify(user=userdata, status="succesful log in", check=user.to_dict())
+    response = make_response(redirect("/register/certificate"))
+    response.set_cookie("check", "cookie-set")
+    return response
+
+
+@app_views.route("/register/<level>", methods=['GET', 'POST'])
+def registeration_form(level):
+    """returns registration form"""
+    return render_template(f"{level}-sign-up.html")
